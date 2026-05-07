e781b854d00952874ef87c068e70edbe50153da1
Pedro Galembeck <galembeckpedro@gmail.com>
2026-05-06 22:59:19 -0300

    chore(repository): add user and company table migrations

---
+++ Repository/Migrations/20260506204559_UpdateUserAndCompanyTables.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserAndCompanyTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompanySlug",
+                table: "TBCompany",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanySlug",
+                table: "TBCompany");
+        }
+    }
+}
